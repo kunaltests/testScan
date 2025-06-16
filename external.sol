20a4ef3c5d07aa485613c4e89d7eb712a65e7c39
kunaltests <116972038+kunaltests@users.noreply.github.com>
2025-06-16 16:14:57 +0530

    Add files via upload

---
+++ external.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.8 <0.8.0;
+
+contract test{
+    address owner;
+    function testthis(address _owner) public {
+        owner = _owner;
+    }
+}
